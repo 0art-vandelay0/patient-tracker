0597f76e79f504bd7596d088b3608cac862b24e0
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-31 11:16:04 -0700

    Add controller template

---
+++ docOffice/Controllers/SampleController.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using docOffice.Models;
+
+namespace docOffice.Controllers
+{
+    public class docOffice : Controller
+    {
+    }
+}
