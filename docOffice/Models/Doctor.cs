f4f29b99be2bca53782eb98ac70494efef837cb9
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-31 13:12:24 -0700

    Stage Doctor model

---
+++ docOffice/Models/Doctor.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace DocOffice.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+        public string Name { get; set; }
+    }
+}
