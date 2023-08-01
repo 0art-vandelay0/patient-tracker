b5f198273b711cf581aea36fcf7900d3b60d9e1a
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-08-01 09:53:13 -0700

    Add Patient details

--- docOffice/Controllers/PatientsController.cs
+++ docOffice/Controllers/PatientsController.cs
@@ -9,6 +9,23 @@ namespace DocOffice.Controllers
 {
     public class PatientsController : Controller
     {
+        private readonly DocOfficeContext _db;
 
+        public PatientsController(DocOfficeContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Patient> model = _db.Patients.ToList();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Patient thisPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
+            return View(thisPatient);
+        }
     }
 }
